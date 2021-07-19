54bf69043a302c0f0dc01831a99e2f0e893ffd65
tszydlo <tszydlo@agh.edu.pl>
2021-07-19 18:18:11 +0200

    Organized files for RES example 1.

---
+++ examples/org/cloudbus/cloudsim/osmesis/examples/RES_example1.java
@@ -0,0 +1,136 @@
+/*
+ * Title:        IoTSim-Osmosis 1.0
+ * Description:  IoTSim-Osmosis enables the testing and validation of osmotic computing applications
+ * 			     over heterogeneous edge-cloud SDN-aware environments.
+ *
+ * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * Copyright (c) 2020, Newcastle University (UK) and Saudi Electronic University (Saudi Arabia)
+ *
+ */
+
+package org.cloudbus.cloudsim.osmesis.examples;
+
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.edge.core.edge.ConfiguationEntity;
+import org.cloudbus.cloudsim.edge.core.edge.MEL;
+import org.cloudbus.cloudsim.edge.utils.LogUtil;
+import org.cloudbus.cloudsim.osmesis.examples.uti.LogPrinter;
+import org.cloudbus.cloudsim.osmesis.examples.uti.PrintResults;
+import org.cloudbus.cloudsim.sdn.Switch;
+import org.cloudbus.osmosis.core.*;
+import org.cloudbus.res.EnergyController;
+import org.cloudbus.res.config.AppConfig;
+import org.cloudbus.res.dataproviders.res.RESResponse;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class RES_example1 {
+    public static final String configurationFile = "inputFiles/res/RES_example1_infrastructure.json";
+    public static final String osmesisAppFile =  "inputFiles/res/RES_example1_workload.csv";
+    public static final String RES_CONFIG_FILE =  "inputFiles/res/RES_example1_energy_config.json";
+
+    OsmosisBuilder topologyBuilder;
+    OsmesisBroker osmesisBroker;
+    List<OsmesisDatacenter> datacenters;
+    List<MEL> melList;
+    EdgeSDNController edgeSDNController;
+    List<Vm> vmList;
+
+    public static void main(String[] args) throws Exception {
+        RES_example1 osmosis = new RES_example1();
+        osmosis.start();
+    }
+
+    public void start() throws Exception{
+
+        int num_user = 1; // number of users
+        Calendar calendar = Calendar.getInstance();
+        boolean trace_flag = false; // mean trace events
+
+        // Initialize the CloudSim library
+        CloudSim.init(num_user, calendar, trace_flag);
+        osmesisBroker  = new OsmesisBroker("OsmesisBroker");
+        topologyBuilder = new OsmosisBuilder(osmesisBroker);
+        ConfiguationEntity config = buildTopologyFromFile(configurationFile);
+        //
+        if(config !=  null) {
+            topologyBuilder.buildTopology(config);
+        }
+
+        OsmosisOrchestrator maestro = new OsmosisOrchestrator();
+
+        OsmesisAppsParser.startParsingExcelAppFile(osmesisAppFile);
+        List<SDNController> controllers = new ArrayList<>();
+        for(OsmesisDatacenter osmesisDC : topologyBuilder.getOsmesisDatacentres()){
+            osmesisBroker.submitVmList(osmesisDC.getVmList(), osmesisDC.getId());
+            controllers.add(osmesisDC.getSdnController());
+            osmesisDC.getSdnController().setWanOorchestrator(maestro);
+        }
+        controllers.add(topologyBuilder.getSdWanController());
+        maestro.setSdnControllers(controllers);
+        osmesisBroker.submitOsmesisApps(OsmesisAppsParser.appList);
+        osmesisBroker.setDatacenters(topologyBuilder.getOsmesisDatacentres());
+
+        Map<String, EnergyController> energyControllers = getEnergyControllers();
+        System.out.println(energyControllers);
+
+        double startTime = CloudSim.startSimulation();
+
+        LogUtil.simulationFinished();
+        PrintResults pr = new PrintResults();
+        pr.printOsmesisNetwork();
+
+        Log.printLine();
+
+        for(OsmesisDatacenter osmesisDC : topologyBuilder.getOsmesisDatacentres()){
+            List<Switch> switchList = osmesisDC.getSdnController().getSwitchList();
+            LogPrinter.printEnergyConsumption(osmesisDC.getName(), osmesisDC.getSdnhosts(), switchList, startTime);
+            Log.printLine();
+        }
+
+        Log.printLine();
+        LogPrinter.printEnergyConsumption(topologyBuilder.getSdWanController().getName(), null, topologyBuilder.getSdWanController().getSwitchList(), startTime);
+        Log.printLine();
+        Log.printLine("Simulation Finished!");
+
+    }
+
+    private Map<String, EnergyController> getEnergyControllers() throws IOException {
+        RESResponse resResponse = AppConfig.RES_PARSER.parse(RES_CONFIG_FILE);
+        return resResponse.getDatacenters()
+                .stream()
+                .map(EnergyController::fromDatacenter)
+                .collect(Collectors.toMap(EnergyController::getEdgeDatacenterId, Function.identity()));
+    }
+
+    private ConfiguationEntity buildTopologyFromFile(String filePath) throws Exception {
+        System.out.println("Creating topology from file " + filePath);
+        ConfiguationEntity conf  = null;
+        try (FileReader jsonFileReader = new FileReader(filePath)){
+            conf = topologyBuilder.parseTopology(jsonFileReader);
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: input configuration file not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Topology built:");
+        return conf;
+    }
+
+    public void setEdgeSDNController(EdgeSDNController edc) {
+        this.edgeSDNController = edc;
+    }
+}
