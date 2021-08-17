71041b7fdc9363c165326f14b8517d34a5397bf2
nazkord <nazkord@gmail.com>
2021-08-17 22:03:32 +0200

    load policy by className

--- sources/org/cloudbus/res/policies/EnergyManagementPolicy.java
+++ sources/org/cloudbus/res/policies/EnergyManagementPolicy.java
@@ -1,8 +1,19 @@
 package org.cloudbus.res.policies;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 
 @Data
-public class EnergyManagementPolicy {
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        property = "className",
+        visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ExportLimitEnergyPolicy.class, name = "ExportLimitEnergyManagementPolicy"),
+        @JsonSubTypes.Type(value = ExportLimitWithEnergyStoragePolicy.class, name = "ExportLimitWithEnergyStoragePolicy"),
+        @JsonSubTypes.Type(value = ExportLimitWithoutEnergyStoragePolicy.class, name = "ExportLimitWithoutEnergyStoragePolicy")
+})
+public abstract class EnergyManagementPolicy {
     private String className;
 }
---
+++ sources/org/cloudbus/res/policies/ExportLimitWithoutEnergyStoragePolicy.java
@@ -0,0 +1,7 @@
+package org.cloudbus.res.policies;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ExportLimitWithoutEnergyStoragePolicy extends EnergyManagementPolicy {
+}
